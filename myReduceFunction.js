eac48c217346aebf147b70c32e1d0924efb82968
DavidkaneZedster <91973292+DavidkaneZedster@users.noreply.github.com>
2022-10-29 18:14:55 +0300

    Create myReduceFunction.js

---
+++ myReduceFunction.js
@@ -0,0 +1,23 @@
+const workersSalary = [
+    {name: 'David', salary: 35000},
+    {name: 'Roman', salary: 50000},
+    {name: 'Alexander', salary: 15000},
+]
+
+const myReduceFunction = (data, callback, initialValue) => {
+  let acc = initialValue;
+  for (const item of data) {
+    acc = callback(acc, item);
+  }
+  return acc;
+};
+
+const myReduceResult = myReduceFunction(
+  workersSalary,
+  (acc, worker) => (worker.salary > acc.salary ? worker : acc),
+  workersSalary[0],
+)
+  
+const reduceResult = workersSalary.reduce((acc, worker) => worker.salary > acc.salary ? worker : acc)
+
+console.log('myReduceOutputValue = ', myReduceResult, '\nReduceOutPutValue =', reduceResult)
